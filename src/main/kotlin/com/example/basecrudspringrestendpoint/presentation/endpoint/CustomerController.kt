2b3b6242e5b49bde6a1fd221a78b73129a2a2fd5
Viktor Befort <v.befort@googlemail.com>
2019-09-23 23:42:27 +0200

    fix import

--- src/main/kotlin/com/example/basecrudspringrestendpoint/presentation/endpoint/CustomerController.kt
+++ src/main/kotlin/com/example/basecrudspringrestendpoint/presentation/endpoint/CustomerController.kt
@@ -1,9 +1,9 @@
 package com.example.basecrudspringrestendpoint.presentation.endpoint
 
 import com.example.basecrudspringrestendpoint.data.model.CustomerEntity
-import com.example.basecrudspringrestendpoint.presentation.mapper.CustomerMapperImpl
 import com.example.basecrudspringrestendpoint.presentation.dto.CustomerDto
 import com.example.basecrudspringrestendpoint.presentation.endpoint.core.BaseCrudController
+import com.example.basecrudspringrestendpoint.presentation.mapper.CustomerMapper
 import com.example.basecrudspringrestendpoint.service.CustomerService
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,4 +11,4 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 @RequestMapping("/api/v1/customer")
 class CustomerController(override val service: CustomerService,
-                         override val mapper: CustomerMapperImpl) : BaseCrudController<CustomerEntity, Long, CustomerDto>()
+                         override val mapper: CustomerMapper) : BaseCrudController<CustomerEntity, Long, CustomerDto>()
--- src/main/kotlin/com/example/basecrudspringrestendpoint/presentation/endpoint/LabourerController.kt
+++ src/main/kotlin/com/example/basecrudspringrestendpoint/presentation/endpoint/LabourerController.kt
@@ -1,9 +1,9 @@
 package com.example.basecrudspringrestendpoint.presentation.endpoint
 
 import com.example.basecrudspringrestendpoint.data.model.LabourerEntity
-import com.example.basecrudspringrestendpoint.presentation.mapper.LabourerMapperImpl
 import com.example.basecrudspringrestendpoint.presentation.dto.LabourerDto
 import com.example.basecrudspringrestendpoint.presentation.endpoint.core.BaseCrudController
+import com.example.basecrudspringrestendpoint.presentation.mapper.LabourerMapper
 import com.example.basecrudspringrestendpoint.service.LabourerService
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,5 +11,5 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 @RequestMapping("/api/v1/labourer")
 class LabourerController(override val service: LabourerService,
-                         override val mapper: LabourerMapperImpl) : BaseCrudController<LabourerEntity, Long, LabourerDto>() {
+                         override val mapper: LabourerMapper) : BaseCrudController<LabourerEntity, Long, LabourerDto>() {
 }
